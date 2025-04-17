2c5ecc79e7a8d679c4ebd1f248183f47dcdfc3d7
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2025-04-17 13:49:10 +0100

    added first screen, which is the home screen. it will be the first navigation route

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,21 @@
+// react
+import React from "react"
+
+// native
+import { View, Text, StyleSheet } from "react-native"
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>This is the Home Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
